5bead929e6bf6e17de95853e3b22175bbf83720e
0rigine <malo.puissant.20160347@efrei.net>
2017-10-11 16:08:40 +0200

    None

--- Citysim/City.h
+++ Citysim/City.h
@@ -15,9 +15,9 @@ public:
 
 	// Contrats
 	int parcourirContrats(); // renvoie une erreur au besoin
-	int makeADeal(City const& associate, int timeleft, float &resourceUsed, float cost, float quantity); // création d'une structure contrat ajoutée à la liste de la ville active
-	bool endADeal(City const& associate, contrat &deal); // supprime le contrat indiqué
-	bool honorTheDeal();
+	int makeADeal(City *associate, int timeleft, float &resourceUsed, float cost, float quantity); // création d'une structure contrat ajoutée à la liste de la ville active
+	bool endADeal(City *associate); // supprime le contrat indiqué
+	bool honorTheDeal(City *associate, contrat &deal); // honorer le contrat
 
 
 protected:
