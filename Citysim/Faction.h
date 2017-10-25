a465dbd45aacd5b74a1d5cb1905f3a4c6eea3e40
0rigine <malo.puissant.20160347@efrei.net>
2017-10-25 19:15:16 +0200

    Création de la classe faction

---
+++ Citysim/Faction.h
@@ -0,0 +1,13 @@
+#pragma once
+class Faction
+{
+public:
+	Faction();
+	~Faction();
+
+private:
+	float nourriture; // stock de nourriture
+	float energie; // stock d'énergie
+	float budget; // argent disponible
+};
+
