c890135a5e012e0e5631d63320b7917afef756d4
0rigine <malo.puissant.20160347@efrei.net>
2017-11-04 16:18:38 +0100

    Utilisation d'itérateurs pour la grille de villes (WIP)

--- Citysim/Citysim.cpp
+++ Citysim/Citysim.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
 	srand(time(NULL));
-	vector< vector<City>> test = Grille::initialize_Grid(1, 1);
-	test[0].push_back(City());
-	test[0][0].acheterVille(test[0][1]);
+	vector< vector<City*>> test = Grille::initialize_Grid(1, 1);
+	//test[0].push_back(new City());
+	//test[0][0]->acheterVille(test[0][1]);
 
 	system("pause");
     return 0;
--- Citysim/Grille.cpp
+++ Citysim/Grille.cpp
@@ -16,13 +16,14 @@ Grille::~Grille()
 vector<vector<City*>> Grille::initialize_Grid(int sizex, int sizey)
 {
 	vector<vector<City*>> grille;
-	vector::iterator i,j;
+	vector<vector<City*>>::iterator i;
+	vector<City*>::iterator j;
 	for (i = grille.begin(); i != grille.end(); ++i)
 	{
-		grille.push_back(vector<City>(0));
-		for (j = (*i)->begin(); j != (*i)->end(); ++j)
+		grille.push_back(vector<City*>(0));
+		for (j = i->begin(); j != i->end(); ++j)
 		{
-			(*i)->push_back(new City());
+			i->push_back(new City());
 		}
 	}
 
