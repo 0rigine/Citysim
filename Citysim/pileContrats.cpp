b267649ae3f1ea0e1fd39f3d21036e7e056c87c4
0rigine <malo.puissant.20160347@efrei.net>
2017-11-27 17:08:42 +0100

    Objet pileContrats

---
+++ Citysim/pileContrats.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "pileContrats.h"
+
+
+pileContrats::pileContrats()
+{
+}
+
+
+pileContrats::~pileContrats()
+{
+}
---
+++ Citysim/pileContrats.h
@@ -0,0 +1,8 @@
+#pragma once
+class pileContrats
+{
+public:
+	pileContrats();
+	~pileContrats();
+};
+
